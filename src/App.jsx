e9a9cd98a8bfbc949ce8ea0cd9b570b2904fb0cb
Nathan <nathan.froeh@gmail.com>
2019-07-14 13:33:54 -0600

    having some kind of hook error

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {useSelector, useDispatch} from 'react-redux';
 import {increment, decrement} from './actions/index'
 
 
-function App() {
-  const counter = useSelector(state => state.counter)
-  const isLogged = useSelector(state => state.logged)
-  const dispatch = useDispatch()
-  return (
-    <div className="App">
-      <h1>Counter {counter}</h1>
-      <button onClick={() => dispatch(increment(2))}>+</button>
-      <button onClick={() => dispatch(decrement(1))}>-</button>
-      {isLogged && <h3>is logged in</h3>}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      idea: '',
+      counter: useSelector(state => state.counter),
+      dispatch: useDispatch()
+    }
+  }
+
+  updateIdea = (e) => {
+    console.log(e.target.value)
+  }
+
+  submitIdea = (e) => {
+    e.preventDefault()
+  }
+
+  // counter = () => useSelector(state => state.counter)
+  // dispatch = () => useDispatch()
+
+  render(){
+  // const isLogged = useSelector(state => state.logged)
+
+    return (
+      <div className="App">
+        <h1>Counter {this.counter}</h1>
+        {/* <button onClick={() => this.dispatch(increment(2))}>+</button>
+        <button onClick={() => this.dispatch(decrement(1))}>-</button> */}
+        <form action="add-idea" onSubmit={this.submitIdea}>
+          <input type="text" name='idea' onChange={this.updateIdea}/>
+          <input type="submit"/>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default App;
